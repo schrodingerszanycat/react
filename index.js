adada5475e0caab7daa826a677b9900ab974f2c6
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2023-10-17 11:35:41 +0530

    Add

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
 
-/* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
 
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
-*/
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
 
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+// document.getElementById("root").append(JSON.stringify(page)); 
+// JSX returns plain javascript objects
+ReactDOM.render(page, document.getElementById("root"))
+// you have to use render - interprets them as react elements that 
+// the browser can understand
